0b4cfe51bc466b0783731d54c5306daa57a44bee
bettitaksas <109687064+bettitaksas@users.noreply.github.com>
2023-12-21 08:42:40 +0100

    initialize Spring Boot project

---
+++ src/main/java/com/oh/votingdatamanager/VotingDataManagerApplication.java
@@ -0,0 +1,13 @@
+package com.oh.votingdatamanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingDataManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VotingDataManagerApplication.class, args);
+    }
+
+}
