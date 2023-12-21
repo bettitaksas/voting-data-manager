95b28595852a749812e891b9db65454b9365a919
bettitaksas <109687064+bettitaksas@users.noreply.github.com>
2023-12-21 12:03:15 +0100

    create enums for storing types

---
+++ src/main/java/com/oh/votingdatamanager/Model/VoteOption.java
@@ -0,0 +1,10 @@
+package com.oh.votingdatamanager.Model;
+
+public enum VoteOption {
+    I("igen"),
+    N("nem"),
+    T("tartózkodás");
+
+    VoteOption(String nameOfVoteOption) {
+    }
+}
---
+++ src/main/java/com/oh/votingdatamanager/Model/VotingType.java
@@ -0,0 +1,10 @@
+package com.oh.votingdatamanager.Model;
+
+public enum VotingType {
+    J("jelenlét"),
+    E("egyszerű többségi szavazás"),
+    M("minősített többségi szavazás");
+
+    VotingType(String nameOfVotingType) {
+    }
+}
